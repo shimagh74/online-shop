3e22c68026315326dace5ce35fc922ae3e20dedd
shimagh74 <shima.ghasemzade92@gmail.com>
2024-02-20 13:54:41 +0330

    improve responsive

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -37,8 +37,8 @@ const Home = () => {
                 </div>
             </div>
             <div className='md:w-10/12 w-full grid grid-cols-3 gap-3 px-3 md:gap-6 mx-auto justify-center py-9'>
-                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-12 h-12 object-contain text-red-500">
+                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center sm:gap-5'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-12 h-12 object-contain hidden sm:block text-red-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                     </svg>
                     <div >
@@ -46,8 +46,8 @@ const Home = () => {
                         <p className='text-gray-500 text-sm'>order over $200</p>
                     </div>
                 </div>
-                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-12 h-12 object-contain text-red-500">
+                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center sm:gap-5'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-12 h-12 object-contain hidden sm:block text-red-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                     </svg>
 
@@ -56,8 +56,8 @@ const Home = () => {
                         <p className='text-gray-500 text-sm'>7 Days money return</p>
                     </div>
                 </div>
-                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-                    <BiSupport className="w-12 h-12 object-contain text-red-500" />
+                <div className='border border-red-400 rounded-sm px-3 py-6 flex justify-center items-center sm:gap-5'>
+                    <BiSupport className="w-12 h-12 object-contain text-red-500 hidden sm:block" />
                     <div >
                         <h4 className='font-medium capitalize text-lg'>24/7 Support</h4>
                         <p className='text-gray-500 text-sm'>Customer support</p>
